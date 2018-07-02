6698073936e8aba891a10b909a957633bb115211
Jonathan Jacobs <jonathan@jsphere.com>
2018-07-02 13:16:52 +0200

    Use Versioneer.

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
+import os
+import codecs
+import versioneer
 from setuptools import setup
 
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(os.path.join(HERE, *parts), 'rb', 'utf-8') as f:
+        return f.read()
+
+
 setup(
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     name='txdocumint',
-    version='17.0.0',
     description='Twisted Python clj-documint client implementation',
+    license='MIT',
     author='Jonathan Jacobs',
     author_email='jonathan@jsphere.com',
     url='https://github.com/fusionapp/txdocumint',
     platforms='any',
-    license='MIT',
+    long_description=read('README.rst'),
     packages=['txdocumint', 'txdocumint.test'],
     test_suite='txdocumint',
     zip_safe=False,
